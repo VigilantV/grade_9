8ace3f93c0f3969fbf0bb940d6104c60b9450921
Mehrab Gheibi <mehrabgheibi@gmail.com>
2023-10-17 00:57:52 +0330

    ref: update deadline

--- src/assets/data files/challengesInfo.jsx
+++ src/assets/data files/challengesInfo.jsx
@@ -1,5 +1,5 @@
 import Challenge_1 from "../../components/challenges/Challenge_1";
 
-const challengesInfo = [{ content: <Challenge_1 />, deadline: "1402/7/24" }];
+const challengesInfo = [{ content: <Challenge_1 />, deadline: "1402/8/1" }];
 
 export default challengesInfo;
