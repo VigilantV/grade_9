01e2ac7b5f32f7ccb01e54065c8433edd868250a
Mehrab Gheibi <mehrabgheibi@gmail.com>
2023-11-16 11:56:17 +0330

    add challenge_3

--- src/assets/data files/challengesInfo.jsx
+++ src/assets/data files/challengesInfo.jsx
@@ -1,9 +1,11 @@
 import Challenge_1 from "../../components/challenges/Challenge_1";
 import Challenge_2 from "../../components/challenges/Challenge_2";
+import Challenge_3 from "../../components/challenges/Challenge_3";
 
 const challengesInfo = [
   { content: <Challenge_1 />, deadline: "1402/8/1" },
   { content: <Challenge_2 />, deadline: "1402/8/8" },
+  { content: <Challenge_3 />, deadline: "1402/9/6" },
 ];
 
 export default challengesInfo;
