27ad5e8bbd96e70c8cad0599d7ede484272a3a18
Mehrab Gheibi <mehrabgheibi@gmail.com>
2023-11-30 16:53:59 +0330

    edit project 1

--- src/assets/data files/challengesInfo.jsx
+++ src/assets/data files/challengesInfo.jsx
@@ -5,7 +5,7 @@ import Project_1 from "../../components/challenges/Project_1";
 const challengesInfo = [
   { content: <Challenge_1 />, name:"Challenge_1", deadline: "1402/8/1" },
   { content: <Challenge_2 />, name:"Challenge_2", deadline: "1402/8/8" },
-  { content: <Project_1 />, name:"Project_1", deadline: "1402/9/6" },
+  { content: <Project_1 />, name:"Project_1", deadline: "1402/9/24" },
 ];
 
 export default challengesInfo;
